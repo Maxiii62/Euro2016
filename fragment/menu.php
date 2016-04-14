2ebeee3b272bff25b22ce3f4fe45230f72a2f430
acoussemaeker <arnaud.cousco@hotmail.fr>
2016-04-14 18:01:05 +0200

    [vue] correction bug

--- fragment/menu.php
+++ fragment/menu.php
@@ -1,24 +1,23 @@
-<?php
-  echo '
+
   <nav class="white" role="navigation">
     <div class="nav-wrapper container">
         <img src="materialize/img/UEFA_Euro_2016_logo.png" alt="Logo Euro 2016 - FRANCE">
 
       <ul class="right hide-on-med-and-down">
-        <li class="userInfos">'.$_SESSION['monUserCo']['nom'].' '. $_SESSION['monUserCo']['prenom'].' - 0 point<li>
+          <?php
+        echo '<li class="userInfos">'.$_SESSION['monUserCo']['nom'].' '. $_SESSION['monUserCo']['prenom'].' - 0 point<li>';
+                ?>
+          <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="GestionUser.php">Gestion Utilisateur</a></li>
+    <li><a href="GestionMatch.php">Gestion Match</a></li>
+    <li><a href="Statistique.php">Statistique</a></li>
+  </ul>
         <li><a href="home.php">Accueil</a></li>
         <li><a href="mesParis.php">Mes paris</a></li>
-        <li><a href="#">Panel Admin</a></li>
+         <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Panel Admin<i class="material-icons right">arrow_drop_down</i></a></li>
         <li><a href="logout.php">Déconnexion</a></li>
       </ul>
 
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Accueil</a></li>
-        <li><a href="#">Mes paris</a></li>
-        <li><a href="#">Panel Admin</a></li>
-        <li><a href="#">Déconnexion</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+   
     </div>
-  </nav>';
-?>
+  </nav>

d8dbc9f0388bf427a1666bcc4eeff56a79e8f0f2
Maxiii62 <maxime.roussel1@epsi.fr>
2016-04-14 13:54:05 +0200

    [Vue] Modification Affichage matchs

--- home.php
+++ home.php
@@ -10,24 +10,51 @@
   <div class="row">
     <div class="col s12">
       <ul class="tabs">
-        <li class="tab col s3"><a href="home.php">POULE A</a></li>
-        <li class="tab col s3"><a class="active" href="home.php">POULE B</a></li>
-        <li class="tab col s3"><a href="home.php">POULE C</a></li>
-        <li class="tab col s3"><a href="home.php">Test 4</a></li>
+        <li class="tab col s3"><a href="#test1">POULE A</a></li>
+        <li class="tab col s3"><a class="active" href="#test2">POULE B</a></li>
+        <li class="tab col s3"><a href="#test3">POULE C</a></li>
+        <li class="tab col s3"><a href="#test4">POULE D</a></li>
       </ul>
     </div>
-    <!--<div id="test1" class="col s12">Test 1</div>
-    <div id="test2" class="col s12">Test 2</div>
-    <div id="test3" class="col s12">Test 3</div>
-    <div id="test4" class="col s12">Test 4</div>-->
+    <div id="test1" class="col s12">
+      <h3>Match du 14/04/2016 - POULE A </h3>
+      <?php for($x = 0 ; $x < rand ( 0 , 4);$x++){
+                echo '<div class="row">';
+                  for($i = 0;$i < rand ( 0 , 3 );$i++){ include 'fragment/pari.php'; }
+                echo '</div>';
+              }
+      ?>
+    </div>
+
+    <div id="test2" class="col s12">
+      <h3>Match du 14/04/2016 - POULE B </h3>
+      <?php for($x = 0 ; $x < rand ( 0 , 4);$x++){
+                echo '<div class="row">';
+                  for($i = 0;$i < rand ( 0 , 3 );$i++){ include 'fragment/pari.php'; }
+                echo '</div>';
+              }
+      ?>
+    </div>
+    <div id="test3" class="col s12">
+      <h3>Match du 14/04/2016 - POULE C </h3>
+      <?php for($x = 0 ; $x < rand ( 0 , 4);$x++){
+                echo '<div class="row">';
+                  for($i = 0;$i < rand ( 0 , 3 );$i++){ include 'fragment/pari.php'; }
+                echo '</div>';
+              }
+      ?>
+    </div>
+    <div id="test4" class="col s12">
+      <h3>Match du 14/04/2016 - POULE D </h3>
+      <?php for($x = 0 ; $x < rand ( 0 , 4);$x++){
+                echo '<div class="row">';
+                  for($i = 0;$i < rand ( 0 , 3 );$i++){ include 'fragment/pari.php'; }
+                echo '</div>';
+              }
+      ?>
+    </div>
   </div>
-    <h3>Match du 14/04/2016 - POULE A </h3>
-    <?php for($x = 0 ; $x < rand ( 0 , 4);$x++){
-              echo '<div class="row">';
-                for($i = 0;$i < rand ( 0 , 3 );$i++){ include 'fragment/pari.php'; }
-              echo '</div>';
-            }
-    ?>
+
 
 
 
